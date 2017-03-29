69b957c73082d38e79eb54a31dad2c775240dac0
Kiril Chomaniuk <Kiril Chomaniuk>
2017-03-29 22:38:29 +0300

    added Activity and Criteria get/set methods and javadoc

--- src/studyplanner/Model/Activity.java
+++ src/studyplanner/Model/Activity.java
@@ -1,5 +1,107 @@
 package studyplanner.Model;
 
+import java.util.ArrayList;
+
 public class Activity {
-    
+
+    private String name;                        //Name of an activity.
+    private String description;                 //Description of an activity.
+    private Double value;                       //Value of an activity.
+    //TODO check if plural of 'criteria' exists
+    private ArrayList<Criteria> criterias;      //List of all criterias of an activity.
+    /**
+     * Default constructor for an instance of Activity.
+     */
+    public Activity() {
+        //TODO put checks in place for unexpected behaviour - make error message shown in GUI and program not terminate
+        this.name = null;
+        this.description = null;
+        this.value = 0.0;
+        this.criterias = new ArrayList<Criteria>();
+    }
+
+    /**
+     * Update a criteria.
+     */
+    public void updateCriteria(Criteria c){
+
+        c.setMet(true);
+        //TODO check if the method has to do something besides setting boolean isMet
+    }
+
+    /*******************
+     * GET/SET METHODS *
+     *******************/
+
+    /**
+     * Get name of an activity.
+     * @return Activity name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set name of an activity.
+     * @param name Name for an activity.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get description of an activity.
+     * @return  Activity description.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Set description of an activity.
+     * @param description Description for an activity.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Get the value of an activity.
+     * @return Activity value.
+     */
+    public Double getValue() {
+        return value;
+    }
+
+    /**
+     * Set the value of an activity.
+     * @param value Value for an activity.
+     */
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    /**
+     * Get a list of criterias of an activity.
+     * @return List of activity criterias.
+     */
+    public ArrayList<Criteria> getCriterias() {
+        return criterias;
+    }
+
+    /**
+     * Add a criteria to an activity.
+     * @param criteria Criteria to add for an activity.
+     */
+    public void add(Criteria criteria){
+        criterias.add(criteria);
+    }
+
+    /**
+     * Add an existing list of criterias to the activity.
+     * @param criterias Existing list of criterias to add for an activity.
+     */
+    public void add(ArrayList<Criteria> criterias) {
+        this.criterias = criterias;
+    }
 }
--- src/studyplanner/Model/Criteria.java
+++ src/studyplanner/Model/Criteria.java
@@ -1,8 +1,113 @@
 package studyplanner.Model;
 
 public class Criteria {
-    public enum CriteriaType{
-        Bool, Value
+
+    private String name;             //Name of a criteria.
+    private CriteriaType type;       //Type of a criteria.
+    private boolean isMet;           //Whether criteria is met or not.
+    private double value;            //Value of a criteria.
+    private String unitOfMeasure;    //Unit of measure for the criteria.
+
+    /**
+     * Default constructor for an instance of Criteria.
+     */
+    public Criteria(){
+        //TODO put checks in place for unexpected behaviour - make error message shown in GUI and program not terminate
+        this.name = null;
+        this.type = null;
+        this.isMet = false;
+        this.value = 0.0;
+        this.unitOfMeasure= null;
     }
-    
+
+    /**
+     * Update a task.
+     */
+    public void updateTask(){
+        //TODO update the task that a criteria has been completed - note: could potentially move this to Task class
+    }
+
+    /*******************
+     * GET/SET METHODS *
+     *******************/
+
+    /**
+     * Get name of a criteria.
+     * @return Criteria name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set name of a criteria.
+     * @param name Name for a criteria.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get type of a criteria.
+     * @return Criteria type.
+     */
+    public CriteriaType getType() {
+        return type;
+    }
+
+    /**
+     * Set type of a criteria.
+     * @param type Type for a criteria.
+     */
+    public void setType(CriteriaType type) {
+        this.type = type;
+    }
+
+    /**
+     * Check if the criteria is met.
+     * @return Whether the criteria is met or not.
+     */
+    public boolean isMet() {
+        return isMet;
+    }
+
+    /**
+     * Set the criteria to be met or unmet.
+     * @param met Is the criteria met or not.
+     */
+    public void setMet(boolean met) {
+        isMet = met;
+    }
+
+    /**
+     * Get value of a criteria.
+     * @return Criteria value.
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Set value of a criteria.
+     * @param value Value for a criteria.
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    /**
+     * Get unit of measure of a criteria.
+     * @return Criteria unit of measure.
+     */
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    /**
+     * Set unit of measure of a criteria.
+     * @param unitOfMeasure Unit of measure for a criteria.
+     */
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
 }
