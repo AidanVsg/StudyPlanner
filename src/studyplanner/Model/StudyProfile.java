dd3cd4a5ffe3482f4cb8bc5c2c140f428cd80ab0
Moaz <moaz_141@hotail.com>
2017-04-01 22:26:50 +0100

    Revert "test"
    This reverts commit 880b19e536dd8cea19861d2619bf3aacb383deb0.

--- src/studyplanner/Model/StudyProfile.java
+++ src/studyplanner/Model/StudyProfile.java
@@ -1,9 +1,5 @@
 package studyplanner.Model;
 
-import java.util.ArrayList;
-
 public class StudyProfile {
-    private String name;                    // name of the study prrofile
-    private ArrayList<Module> modules;      //Liat of all modules
     
 }
