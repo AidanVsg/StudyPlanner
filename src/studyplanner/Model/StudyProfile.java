ec156be7539fe1329092230162f81f502df4d0cb
Moaz <moaz_141@hotail.com>
2017-04-01 22:30:40 +0100

    test

--- src/studyplanner/Model/StudyProfile.java
+++ src/studyplanner/Model/StudyProfile.java
@@ -1,5 +1,7 @@
 package studyplanner.Model;
+import java.util.ArrayList;
 
-public class StudyProfile {
-    
+ public class StudyProfile {
+    private String name;                    // name of the study prrofile
+    private ArrayList<Module> modules;      //Liat of all modules
 }
