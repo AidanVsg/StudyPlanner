1d223ada7ac44e50ff078179be8d64f46b4c9f0a
Kiril Chomaniuk <aidan.vsg1@gmail.com>
2017-05-15 15:43:02 +0100

    changed X axis to Date

--- src/studyplanner/View/GanttChartApplicationView.java
+++ src/studyplanner/View/GanttChartApplicationView.java
@@ -16,6 +16,12 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import studyplanner.View.GanttChartView.ExtraData;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
+
+import java.util.Date;
+import java.util.*;
+import java.util.ArrayList;
 
 // TODO: use date for x-axis
 public class GanttChartApplicationView extends Application {
@@ -23,17 +29,21 @@ public class GanttChartApplicationView extends Application {
     @Override public void start(Stage stage) {
 
         stage.setTitle("Gantt Chart");
-
+      
         String[] tasks = new String[] { "Task 1", "Task 2", "Tasks 3", "Task 4"
         , "Task 5"};
+        //ArrayList<Date> dates = new ArrayList<>();
 
-        final NumberAxis xAxis = new NumberAxis();
+        final DateAxis xAxis = new DateAxis(new GregorianCalendar(2017,3,25).getTime(),new GregorianCalendar(2017,4,25).getTime());
         final CategoryAxis yAxis = new CategoryAxis();
 
-       final GanttChartView<Number,String> chart = new GanttChartView<Number,String>(xAxis,yAxis);
+       final GanttChartView<Date,String> chart = new GanttChartView<Date,String>(xAxis,yAxis);
         xAxis.setLabel("Time");
         xAxis.setTickLabelFill(Color.CHOCOLATE);
-        xAxis.setMinorTickCount(1);
+        //xAxis.setMinorTickCount(1);
+        //xAxis.setTickLabelGap(1);
+        xAxis.setTickLabelGap(3);
+        //xAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(dates)));
 
 
 
@@ -49,37 +59,47 @@ public class GanttChartApplicationView extends Application {
 
         task = tasks[0];
         XYChart.Series series1 = new XYChart.Series();
-        series1.getData().add(new XYChart.Data(4, task, new ExtraData( 1, "status-red")));
+        series1.getData().add(new XYChart.Data(new GregorianCalendar(2017,4,6).getTime(), task, new ExtraData( 100, "status-red")));
 
         task = tasks[1];
         XYChart.Series series2 = new XYChart.Series();
-        series2.getData().add(new XYChart.Data(3, task, new ExtraData( 1, "status-green")));
+        series2.getData().add(new XYChart.Data(new GregorianCalendar(2017,4,1).getTime(), task, new ExtraData( 100, "status-green")));
 
         task = tasks[2];
         XYChart.Series series3 = new XYChart.Series();
-        series3.getData().add(new XYChart.Data(2, task, new ExtraData( 1, "status-blue")));
+        series3.getData().add(new XYChart.Data(new GregorianCalendar(2017,4,2).getTime(), task, new ExtraData( 100, "status-blue")));
         
 
         task = tasks[3];
         XYChart.Series series4 = new XYChart.Series();
-        series4.getData().add(new XYChart.Data(1, task, new ExtraData(1, "status-x")));
+        series4.getData().add(new XYChart.Data(new GregorianCalendar(2017,4,4).getTime(), task, new ExtraData(100, "status-x")));
         
         
         
         task = tasks[4];
         XYChart.Series series5 = new XYChart.Series();
-        series5.getData().add(new XYChart.Data(0, task, new ExtraData(1, "status-yellow")));
+        series5.getData().add(new XYChart.Data(new GregorianCalendar(2017,4,5).getTime() , task, new ExtraData(100, "status-green")));
        
         
-        chart.getData().addAll(series1, series2, series3, series4, series5);           
+        chart.getData().addAll(series5, series4, series3, series2, series1);           
 
         //System.out.println(getClass().getResource("."));
         chart.getStylesheets().add(getClass().getResource("GanttChartView.css").toExternalForm());
 
-      
-        Scene scene  = new Scene(chart,620,350);
+        //StackPane stack = new StackPane();
+        ScrollPane sp = new ScrollPane();
+        //
+        
+        
+        //sp.setContent(chart);
+       // stack.getChildren().add(sp);
+        
+        Scene scene  = new Scene(chart,800,600);
+        
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+        //stack.getChildren().add(xAxis);
     }
 
     public static void main(String[] args) {
