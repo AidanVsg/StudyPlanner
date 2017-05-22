4a037768c1d76225f25b5c5f737005cad7688695
Kiril Chomaniuk <aidan.vsg1@gmail.com>
2017-05-22 21:21:13 +0100

    fixed Git errors

--- src/studyplanner/View/GanttChartApplicationView.java
+++ src/studyplanner/View/GanttChartApplicationView.java
@@ -30,7 +30,7 @@ import javafx.scene.Cursor;
 // TODO: use date for x-axis
 public class GanttChartApplicationView extends Application {
 
-<<<<<<< HEAD
+
     private void showGanttChart(Assignment a, Date firstDateToShow, Date lastDateToShow)
     {
         Stage stage = new Stage();
@@ -54,49 +54,33 @@ public class GanttChartApplicationView extends Application {
         
         //final DateAxis xAxis = new DateAxis(new GregorianCalendar(2017,4,9,00,00,00).getTime(),new GregorianCalendar(2017,4,16,00,00,00).getTime());
         final DateAxis xAxis = new DateAxis(firstDateToShow,lastDateToShow);
-=======
-    @Override public void start(Stage stage) {
-
-        stage.setTitle("Gantt Chart");
-      
-        String[] tasks = new String[] { "Task 5", "Task 4", "Tasks 3", "Task 2"
-        , "Task 1"};
-        //ArrayList<Date> dates = new ArrayList<>();
-
-        final DateAxis xAxis = new DateAxis(new GregorianCalendar(2017,3,1,00,00,00).getTime(),new GregorianCalendar(2017,3,8,00,00,00).getTime());
->>>>>>> da3af90e5fd8b2d8cedd4209f67da21ad8bf8906
         final CategoryAxis yAxis = new CategoryAxis();
         
-        List<String> tasknames = new ArrayList<>();
-        
-       final GanttChartView<Date,String> chart = new GanttChartView<Date,String>(xAxis,yAxis);
+        final GanttChartView<Date,String> chart = new GanttChartView<Date,String>(xAxis,yAxis);
         xAxis.setLabel("Date");
         xAxis.setTickLabelFill(Color.CHOCOLATE);
         xAxis.setTickLabelGap(3);
-<<<<<<< HEAD
+
         xAxis.setTickLabelRotation(90);
 
-=======
+
         //xAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList(dates)));
         xAxis.setTickLabelRotation(90);
  //       xAxis.setLowerBound(DateAxis);
->>>>>>> da3af90e5fd8b2d8cedd4209f67da21ad8bf8906
+
         xAxis.setSide(Side.TOP);
         
         yAxis.setLabel("");
         yAxis.setTickLabelFill(Color.CHOCOLATE);
         yAxis.setTickLabelGap(10);
         
-        yAxis.setCategories(FXCollections.<String>observableArrayList(tasknames));
+        //yAxis.setCategories(FXCollections.<String>observableArrayList(tasknames));
 
         chart.setTitle("Study Planner");
         chart.setLegendVisible(false);
         chart.setBlockHeight( 25); //sets depth of block
-<<<<<<< HEAD
 
         float x = 4.2f;
-
-        ArrayList<XYChart.Series> allstuff = new ArrayList<XYChart.Series>();
         
         for (Task t : a.getTasks())
         {
@@ -105,34 +89,8 @@ public class GanttChartApplicationView extends Application {
             series.getData().add(new XYChart.Data(t.getStart(), t.getName(), new ExtraData(time*x,"status-red")));
             chart.getData().add(series);
         }
-=======
-        String task;
-        float x = 103.0f;
-        task = tasks[0];
-        XYChart.Series series1 = new XYChart.Series();
-        series1.getData().add(new XYChart.Data(new GregorianCalendar(2017,3,5,14,00).getTime(), task, new ExtraData( 2*x, "status-red")));
-
-        task = tasks[1];
-        XYChart.Series series2 = new XYChart.Series();
-        series2.getData().add(new XYChart.Data(new GregorianCalendar(2017,3,4).getTime(), task, new ExtraData( x, "status-green")));
-
-        task = tasks[2];
-        XYChart.Series series3 = new XYChart.Series();
-        series3.getData().add(new XYChart.Data(new GregorianCalendar(2017,3,3).getTime(), task, new ExtraData( x, "status-blue")));
-
-        task = tasks[3];
-        XYChart.Series series4 = new XYChart.Series();
-        series4.getData().add(new XYChart.Data(new GregorianCalendar(2017,3,2).getTime(), task, new ExtraData(x, "status-x")));
         
         
-        task = tasks[4];
-        XYChart.Series series5 = new XYChart.Series();
-        series5.getData().add(new XYChart.Data(new GregorianCalendar(2017,3,1,24,00).getTime() , task, new ExtraData(x, "status-green")));
-       
-        
-        chart.getData().addAll(series1, series2, series3, series4, series5);           
->>>>>>> da3af90e5fd8b2d8cedd4209f67da21ad8bf8906
-
         chart.getStylesheets().add(getClass().getResource("GanttChartView.css").toExternalForm());
 
 
@@ -142,7 +100,8 @@ public class GanttChartApplicationView extends Application {
         stage.setResizable(false);
         stage.show();
     }
-    
+
+
     @Override public void start(Stage stage) {
       
        
